5ee2f363730b8fb29ffe3b7849bde64acb705dc6
zhavoronskaya <helenheykey@gmail.com>
2024-11-08 20:59:45 +0000

    refator(): cleanup main

--- src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-export const SocialLinks = {
-  BANDCAMP: "https://zhavoronskaya.bandcamp.com/",
-  SOUNDCLOUD: "https://soundcloud.com/zhavoronskaya",
-  TELEGRAM: "https://t.me/zhavoronoki/",
-  INSTAGRAM: "https://www.instagram.com/zhav0ronskaya/",
-  DISCOGS: "http://discogs.com/ru/artist/6992688-Zhavoronskaya",
-  EMAIL: "zhavoronskaya.public@gmail.com",
-};
